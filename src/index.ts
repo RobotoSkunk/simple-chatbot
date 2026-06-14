4bda966e6ee4dbb2c8601d2af15ec79fda32997d
RobotoSkunk <contact@robotoskunk.com>
2026-06-13 19:17:21 -0600

    Added Elysia

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia';
+
+const authApp = new Elysia()
+	.onBeforeHandle(() =>
+	{
+		console.log('Handled!');
+	})
+	.get('/something', () => new Response('Check the output!'));
+
+const app = new Elysia()
+	.use(authApp)
+	.get('/', () => new Response('Hi there!'));
+
+app.listen(8080, () =>
+{
+	console.log('Running!');
+});
