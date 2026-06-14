55bd39f52efaaf120ec0b83f8ac35730edda82fc
RobotoSkunk <contact@robotoskunk.com>
2026-06-13 22:41:44 -0600

    Added React package

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import { Elysia } from 'elysia';
+import { staticPlugin } from '@elysia/static';
 
 const authApp = new Elysia()
-	.onBeforeHandle(() =>
-	{
-		console.log('Handled!');
-	})
+	.onBeforeHandle(() => console.log('Handled!'))
 	.get('/something', () => new Response('Check the output!'));
 
-const app = new Elysia()
-	.use(authApp)
-	.get('/', () => new Response('Hi there!'));
-
-app.listen(8080, () =>
-{
-	console.log('Running!');
-});
+export const app = new Elysia()
+	.use(
+		await staticPlugin({
+			prefix: '/',
+			assets: 'web',
+			bunFullstack: true,
+		}
+	))
+	.listen(8080, () => console.log('Running!'));
---
+++ web/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App()
+{
+	return (<>
+		<h1>Does this work?</h1>
+		<p>*toc toc* is anyone there?</p>
+	</>);
+}
