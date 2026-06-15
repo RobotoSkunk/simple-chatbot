cb34d7b09071d3cdfb4d0c727808287a04d6b19a
RobotoSkunk <contact@robotoskunk.com>
2026-06-15 15:18:55 -0600

    Removed options section from system tray

--- src/tray.ts
+++ src/tray.ts
@@ -18,9 +18,6 @@ async function startTray(server: Bun.Server<undefined>)
 		await tray.setTooltip('Simple Chatbot');
 
 		await tray.add('action-open', 'Launch UI');
-		await tray.addSubmenu('sub-options', 'Options');
-
-		await tray.add('action-server-stop', 'Stop all connections', { parent_id: 'sub-options' })
 
 		await tray.addSeparator('sp-1');
 		await tray.add('action-quit', 'Quit');
@@ -30,10 +27,6 @@ async function startTray(server: Bun.Server<undefined>)
 			switch (id) {
 				case 'action-open': {
 					await open(`http://127.0.0.1:${server.port}/`);
-					break;
-				}
-				case 'action-server-stop': {
-					await server.stop(true);
 					break;
 				}
 				case 'action-quit': {
