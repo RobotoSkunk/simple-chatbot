37540e1c85a133b5a65c0cf6237d7f8e9a0a479b
RobotoSkunk <contact@robotoskunk.com>
2026-08-14 02:55:01 -0600

    Added Zxcvbn and create password logic

--- web/app/setup/page.tsx
+++ web/app/setup/page.tsx
@@ -1,4 +1,6 @@
 import {
+	useDeferredValue,
+	useEffect,
 	useState,
 } from 'react';
 
@@ -9,8 +11,75 @@ import {
 	type Variants,
 } from 'motion/react';
 
+import {
+	type FeedbackType,
+	type Matcher,
+	type OptionsType,
+	ZxcvbnFactory,
+} from '@zxcvbn-ts/core';
+
+import {
+	matcherPwnedFactory,
+} from '@zxcvbn-ts/matcher-pwned';
+
+import * as zxcvbnCommon from '@zxcvbn-ts/language-common';
+import * as zxcvbnDictionaryEn from '@zxcvbn-ts/language-en';
+import * as zxcvbnDictionaryEsEs from '@zxcvbn-ts/language-es-es';
+
 import '../globals.css';
 import './page.css';
+
+const zxcvbnOptions = {
+	translations: zxcvbnDictionaryEn.translations,
+	dictionary: {
+		...zxcvbnCommon.dictionary,
+		...zxcvbnDictionaryEn.dictionary,
+		...zxcvbnDictionaryEsEs.dictionary,
+		userInputs: [
+			'chatbot',
+			'chat',
+			'bot',
+			'ai',
+			'llm',
+			'ollama',
+			'bun',
+			'javascript',
+			'typescript',
+			'type',
+			'java',
+			'script',
+			'github',
+			'gitlab',
+			'mcp',
+			'server',
+			'local',
+		],
+	},
+	graphs: zxcvbnCommon.adjacencyGraphs,
+	useLevenshteinDistance: true,
+} satisfies OptionsType;
+
+const zxcvbn = new ZxcvbnFactory(zxcvbnOptions, {
+	pwned: matcherPwnedFactory(fetch),
+});
+
+function offlineScoreFromGuesses(guesses: number)
+{
+	if (guesses < 1e8) {
+		return 0;
+	}
+	if (guesses < 1e11) {
+		return 1;
+	}
+	if (guesses < 1e14) {
+		return 2;
+	}
+	if (guesses < 1e17) {
+		return 3;
+	}
+
+	return 4;
+}
 
 const variants = {
 	hide: {
@@ -25,24 +94,24 @@ const variants = {
 
 const scores = [
 	{
-		label: 'Very Insecure',
+		label: `Absurdly Insecure`,
 		color: '#475569',
+	},
+	{
+		label: 'Very Insecure',
+		color: '#f43f5e',
 	},
 	{
 		label: 'Insecure',
 		color: '#f43f5e',
 	},
 	{
-		label: 'Decent',
+		label: 'Fair',
 		color: '#eab308',
 	},
 	{
-		label: 'Good',
+		label: 'Secure',
 		color: '#84cc16',
-	},
-	{
-		label: 'Excelent',
-		color: '#10b981',
 	},
 ];
 
@@ -50,20 +119,52 @@ export default function Setup()
 {
 	const [ section, setSection ] = useState(0);
 
+	const [ password, setPassword ] = useState('');
+	const passwordDeferred = useDeferredValue(password);
+	const [ repeatedPassword, setRepeatedPassword ] = useState('');
+
+	const [ passwordScore, setPasswordScore ] = useState(0);
+	const [ showPassword, setShowPassword ] = useState(false);
+	const [ feedback, setFeedback ] = useState<FeedbackType>({ warning: null, suggestions: [] });
+
 	function changeSection(direction: -1 | 1)
 	{
 		setSection(section + direction);
 	}
 
+	function passwordIsSecure()
+	{
+		return passwordScore >= 3;
+	}
+
+	async function checkPasswordStrength()
+	{
+		const result = await zxcvbn.checkAsync(passwordDeferred);
+		const score = offlineScoreFromGuesses(result.guesses);
+
+		setPasswordScore(score);
+		setFeedback(result.feedback);
+	}
+
+	useEffect(() =>
+	{
+		checkPasswordStrength();
+	}, [ passwordDeferred ]);
+
+
 	const sections = [
 		( // Welcome
 			<>
-				<motion.h1 variants={ variants }>
+				<motion.h1
+					variants={ variants }
+					key='welcome-title'
+				>
 					Welcome to your Personal Chatbot
 				</motion.h1>
 				<motion.button
 					variants={ variants }
 					onClick={ () => changeSection(1) }
+					key='welcome-button'
 				>
 					Next
 				</motion.button>
@@ -71,37 +172,112 @@ export default function Setup()
 		),
 		( // Password form
 			<form>
-				<motion.h1 variants={ variants }>
+				<motion.h1
+					variants={ variants }
+					layout
+				>
 					Let's create a password
 				</motion.h1>
-				<motion.p variants={ variants } style={{ marginBottom: 22 }}>
-					The password will be used to authenticate you and encrypt all your data, so make sure it is unique and secure.
+				<motion.p
+					variants={ variants }
+					style={{ marginBottom: 22 }}
+					layout
+				>
+					This password will be used to authenticate you and encrypt all your data,<br/>
+					so make sure it's unique and secure. <b>Don't lose your password</b>.
 				</motion.p>
-				<motion.div variants={ variants }>
+				<motion.div
+					className='input-field'
+					variants={ variants }
+					layout
+				>
 					<label htmlFor='password-input'>Password</label><br/>
 					<input
 						id='password-input'
 						type='password'
 						placeholder='••••••••••••'
 						aria-label='Password input field'
+
+						onInput={ (ev) => setPassword(ev.currentTarget.value) }
 					/>
 				</motion.div>
-				<motion.div variants={ variants }>
-					<label htmlFor='repeat-password-input'>Repeat password</label><br/>
-					<input
-						id='repeat-password-input'
-						type='password'
-						placeholder='••••••••••••'
-						aria-label='Repeat password input field'
-					/>
-				</motion.div>
-				<motion.button
-					variants={ variants }
-					style={{ marginTop: 22 }}
-					onClick={ () => changeSection(1) }
-				>
-					Next
-				</motion.button>
+				{ passwordDeferred.length > 0 && <>
+					<div style={{ minHeight: 190 }}>
+						<motion.div
+							className='input-field'
+							variants={ variants }
+							layout
+						>
+							<label htmlFor='repeat-password-input'>Repeat password</label><br/>
+							<input
+								id='repeat-password-input'
+								type='password'
+								placeholder='••••••••••••'
+								aria-label='Repeat password input field'
+
+								onInput={ (ev) => setRepeatedPassword(ev.currentTarget.value) }
+							/>
+						</motion.div>
+						<motion.div
+							variants={ variants }
+							className='score-container'
+						>
+							<span className='info-label'>
+								{ scores[passwordScore]!.label }
+							</span>
+							<motion.div className='score'>
+								<motion.div
+									className='bar'
+									animate={{
+										width: `${(passwordScore / (scores.length - 1)) * 100}%`,
+										backgroundColor: scores[passwordScore]!.color,
+									}}
+								/>
+							</motion.div>
+						</motion.div>
+						{ passwordIsSecure() && passwordDeferred !== repeatedPassword &&
+							<motion.p
+								className='warning-label'
+								variants={ variants }
+								layout
+							>
+								The passwords don't match.
+							</motion.p>
+						}
+						{ feedback.warning &&
+							<motion.p
+								className='warning-label'
+								variants={ variants }
+								layout
+							>
+								{ feedback.warning }
+							</motion.p>
+						}
+						<div className='suggestions'>
+							<ul>
+								{ passwordDeferred.length > 0 && feedback.suggestions.map((v, i) =>
+									<motion.li
+										key={ i }
+										variants={ variants }
+										className='info-label'
+										layout
+									>
+										{ v }
+									</motion.li>
+								) }
+							</ul>
+						</div>
+						{ (passwordIsSecure() && passwordDeferred === repeatedPassword) &&
+							<motion.button
+								variants={ variants }
+								style={{ marginTop: 22 }}
+								onClick={ () => changeSection(1) }
+							>
+								Next
+							</motion.button>
+						}
+					</div>
+				</> }
 			</form>
 		),
 	];
